ef8bc9716cf383ce3798548d827d276b052f7f5f
Batoul-Hallaq <btwlhlaq982@gmail.com>
2025-05-19 12:01:37 +0300

    add starting file

---
+++ main.sh
@@ -0,0 +1,9 @@
+# starting Basma's code: Collect Input + Calculate BMR & TDEE
+source myfile.sh
+# Maha's code: Calculate Macronutrients
+source mycarb.sh
+# Batoul's code: Body Mass Index (BMI) Calculator
+source batoul-code.sh
+# Rawaf's code: Track daily calories
+source rawaf-code.sh
+
